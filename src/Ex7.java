910fec618d92d40e064bdad3af270d9000d09253
Sumanth-Raj-Jangeti <sumanthraj239@gmail.com>
2025-12-15 19:50:19 +0530

    Improved Readability

--- src/Ex7.java
+++ src/Ex7.java
@@ -6,14 +6,15 @@ public class Ex7 {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a Binary Number:");
         int binary1=sc.nextInt();
+        int originalbinary=binary1;
         int num1=0,power=0;
         while (binary1>0)
         {
             int rem1=binary1%10;
-            num1+=rem1*Math.pow(2,power);
+            num1+=(rem1*Math.pow(2,power));
             binary1=binary1/10;
             power++;
         }
-        System.out.println(num1);
+        System.out.println("Integer form of "+originalbinary+" is:"+num1);
     }
 }
