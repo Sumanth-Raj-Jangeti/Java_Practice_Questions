5c4dc45f3c7fa2567287488d747b575728153ee2
Sumanth-Raj-Jangeti <sumanthraj239@gmail.com>
2025-12-16 16:57:24 +0530

    This file is added by feature/sumanth

---
+++ src/Ex11.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ex11 {
+    public static void main(String[] args) {
+        //A program to find the factorial of a number using recursion.
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int num=sc.nextInt();
+        System.out.println("Factorial of "+num+" is:"+factorial(num));
+    }
+    public static int factorial(int n)
+    {
+        if(n==0 || n==1)
+            return 1;
+        else
+            return n * factorial(n-1);
+    }
+}
