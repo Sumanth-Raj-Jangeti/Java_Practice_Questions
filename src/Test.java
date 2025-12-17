3762ca08d152416a6a0ae0b2ef18ed70344c115b
Sumanth-Raj-Jangeti <sumanthraj239@gmail.com>
2025-12-17 12:00:06 +0530

    Updated the code as per the review comments

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the values of a,b and c:");
+        System.out.println("Enter the 3 values:");
         int num1=sc.nextInt();
         int num2 =sc.nextInt();
         int num3=sc.nextInt();
