1e4ccd354315db480286008ebdf2694a638f471f
Sumanth-Raj-Jangeti <sumanthraj239@gmail.com>
2025-12-17 11:54:31 +0530

    Changed the code as per the review comments

--- src/Test.java
+++ src/Test.java
@@ -4,18 +4,28 @@ public class Test {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the values of a,b and c:");
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c=sc.nextInt();
-        System.out.println("Addition of "+a+" and "+b+" is:"+add(10,20));       //Method Call
-        System.out.println("Addition of "+a+","+b+" and "+c+" is:"+add(10,20,30)); //Method Call
+        int num1=sc.nextInt();
+        int num2 =sc.nextInt();
+        int num3=sc.nextInt();
+        System.out.println("Addition of "+num1+" and "+num2+" is:"+findsum(10,20));       //Method Call
+        System.out.println("Addition of "+num1+","+num2+" and "+num3+" is:"+findsum(10,20,30)); //Method Call
+        System.out.println("Difference of "+num1+" and "+num2+" is:"+findsub(10,20));       //Method Call
+        System.out.println("Difference of "+num1+","+num2+" and "+num3+" is:"+findsub(10,20,30)); //Method Call
     }
-    public static int add(int a,int b)
+    public static int findsum(int num1,int num2)
     {
-        return a+b;
+        return num1+num2;
     }
-    public static int add(int a,int b,int c)
+    public static int findsum(int num1,int num2,int num3)
     {
-        return a+b+c;
+        return num1+num2+num3;
+    }
+    public static int findsub(int num1,int num2)
+    {
+        return num1-num2;
+    }
+    public static int findsub(int num1,int num2,int num3)
+    {
+        return num1-num2-num3;
     }
 }
