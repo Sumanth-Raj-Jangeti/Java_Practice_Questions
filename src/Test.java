df0024153c7416ab378b81304860b61108c3fad5
Sumanth-Raj-Jangeti <sumanthraj239@gmail.com>
2025-12-17 11:14:58 +0530

    Added a new file in feature branch

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Java Programming");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the values of a,b and c:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        System.out.println("Addition of "+a+" and "+b+" is:"+add(10,20));       //Method Call
+        System.out.println("Addition of "+a+","+b+" and "+c+" is:"+add(10,20,30)); //Method Call
+    }
+    public static int add(int a,int b)
+    {
+        return a+b;
+    }
+    public static int add(int a,int b,int c)
+    {
+        return a+b+c;
     }
 }
