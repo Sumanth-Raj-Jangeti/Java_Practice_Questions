03ca9990dd1d4675f943a3fd2e44c95429a1cb5c
Sumanth-Raj-Jangeti <sumanthraj239@gmail.com>
2026-02-02 18:58:58 +0530

    Improved Readability

--- src/com/arrays/practice/Sum_Avg.java
+++ src/com/arrays/practice/Sum_Avg.java
@@ -3,15 +3,74 @@ package com.arrays.practice;
 public class Sum_Avg {
     public static void main(String[] args) {
         //A program to find the sum & avg of elements in an array
-        int[] arr={10,20,30,40,50};
+        int[] arr1={10,20,30,40,50};
+        sum_avgAll(arr1);
+        int[] arr2={-3,-2,0,-7,25,12,-11};
+        sum_avgOnlyPositives_Negatives(arr2);
+        int[] arr3={1,4,6,5,2,1,7,2,1,2};
+        sum_avgOnlyDistinct(arr3);
+    }
+    public static void sum_avgAll(int[] arr)
+    {
         int sum=0;
-        double avg=0;
-        for (int i=0;i<arr.length;i++)
-        {
-            sum+=arr[i];
+        double avg;
+        for (int j : arr) {
+            sum += j;
         }
-        System.out.println("Sum of Elements in the array:"+sum);
-        avg=(double) sum/ arr.length;
-        System.out.println("Avg of elements int the array:"+avg);
+        avg=(double) sum/arr.length;
+        System.out.println("Sum of all Elements in the array:"+sum+"\nAvg of all elements in the array:"+avg);
+    }
+    public static void sum_avgOnlyPositives_Negatives(int[] arr)
+    {
+        int p_sum=0,p_count=0,n_sum=0,n_count=0;
+        double p_avg,n_avg;
+        System.out.println("\nPositive No.s:");
+        for(int num:arr) {
+            if (num > 0) {
+                System.out.print(num + " ");
+                p_sum += num;
+                p_count++;
+            }
+        }
+        System.out.println("\nNegative No.s:");
+        for(int num:arr)
+        {
+            if (num<=0){
+                System.out.print(num+" ");
+                n_sum+=num;
+                n_count++;
+            }
+        }
+        p_avg=(double)p_sum/p_count;
+        n_avg=(double)n_sum/n_count;
+        System.out.print("\nSum of +ve Elements:"+p_sum+"\nAvg of +ve Elements:"+ p_avg);
+        System.out.print("\nSum of -ve Elements:"+n_sum+"\nAvg of -ve Elements:"+ n_avg);
+    }
+    public static void sum_avgOnlyDistinct(int[] arr)
+    {
+        System.out.println("\n\nDistinct Elements:");
+        int sum=0,count=0;
+        double avg;
+        for(int i=0;i<arr.length;i++)
+        {
+            boolean isDistinct=true;
+            //Check if arr[i] has appeared before.
+            for(int k=0;k<i;k++)
+            {
+                if(arr[k]==arr[i])
+                {
+                    isDistinct=false;
+                    break;
+                }
+            }
+            if(isDistinct)
+            {
+                System.out.print(arr[i]+" ");
+                sum+=arr[i];
+                count++;
+            }
+        }
+        avg=(double) sum/count;
+        System.out.print("\nSum of Distinct Elements:"+sum+"\nAvg of Distinct Elements:"+avg);
     }
 }
