1b29a87faf2aff4439a3686156377ba09f41fa54
Sumanth-Raj-Jangeti <sumanthraj239@gmail.com>
2026-03-28 18:49:27 +0530

    New File - added explanatory comments to existing code

---
+++ src/com/arrays/practice/Find_First_Last_Position_In_SortedArray_LC33.java
@@ -0,0 +1,48 @@
+package com.arrays.practice;
+
+import java.util.Arrays;
+
+public class Find_First_Last_Position_In_SortedArray_LC33 {
+    public static void main(String[] args) {
+        int[] arr={5,6,6,7,7,8,8,8,8,8,9};
+        int target=8;
+        int[] result=searchRange(arr,target);
+        System.out.println(Arrays.toString(result));
+    }
+    public static int[] searchRange(int[] arr, int target) {
+        int first=findFirst(arr,target);
+        if(first==-1) return new int[]{-1,-1};
+        int last=findLast(arr,target);
+        return new int[]{first,last};
+    }
+    public static int findFirst(int[] arr,int target){
+        int low=0,high=arr.length-1;
+        int first=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==target){
+                first=mid;
+                //Since we are finding 1st occurrence --> mostly this shd be in left part.
+                high=mid-1;
+            }
+            else if(arr[mid]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return first;
+    }
+    public static int findLast(int[] arr,int target){
+        int low=0,high=arr.length-1;
+        int last=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==target){
+                last=mid;
+                //Since we are finding last occurrence --> mostly this shd be in right part.
+                low=mid+1;
+            }
+            else if(arr[mid]<target) low=mid+1;
+            else high=mid-1;
+        }
+        return last;
+    }
+}
--- src/com/arrays/practice/SearchInRotatedSortedArray_LC81.java
+++ src/com/arrays/practice/SearchInRotatedSortedArray_LC81.java
@@ -2,8 +2,8 @@ package com.arrays.practice;
 
 public class SearchInRotatedSortedArray_LC81 {
     public static void main(String[] args) {
-        int[] arr={2,5,6,0,0,1,3};
-        int target=0;
+        int[] arr={3,1,2,3,3,3,3};
+        int target=3;
         System.out.println("target is in arr? "+search(arr,target));
     }
     public static boolean search(int[] arr,int target)
